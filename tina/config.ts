4493cf14d5321f94bfe77501b397a94a30321365
Martin Delille <martin@delille.org>
2023-05-01 16:55:03 +0200

    Add date and layouet fields

--- tina/config.ts
+++ tina/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "tinacms";
+import moment from 'moment';
 
 // Your hosting provider likely exposes this as an environment variable
 const BRANCH = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
@@ -26,6 +27,19 @@ export default defineConfig({
         name: "post",
         label: "Posts",
         path: "_posts",
+        ui: {
+          defaultItem: {
+            date: moment(new Date()).format('YYYY-MM-DD'),
+            layout: "post",
+          },
+          filename: {
+            readOnly: true,
+            slugify: values => {
+              return `${values?.date?.substring(0, 10) ||
+                'xxxx'}-${values?.title?.toLowerCase().replace(/[^\w\s]/gi, '').replace(/ /g, '-').normalize('NFD').replace(/[\u0300-\u036f]/g, "")}`
+            },
+          },
+        },
         fields: [
           {
             type: "string",
@@ -35,10 +49,23 @@ export default defineConfig({
             required: true,
           },
           {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+            required: true,
+          },
+          {
             type: "rich-text",
             name: "body",
             label: "Body",
             isBody: true,
+          },
+          {
+            label: 'Layout',
+            name: 'layout',
+            type: 'string',
+            required: true,
+            ui: { component: 'hidden' },
           },
         ],
       },
